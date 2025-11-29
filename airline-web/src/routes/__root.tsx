acd12a422ac29443a456b4316f545c777ca3ff74
Tyler Fink <tfink419@gmail.com>
2025-11-29 14:58:22 -0700

    Clear devtools code

--- airline-web/src/routes/__root.tsx
+++ airline-web/src/routes/__root.tsx
@@ -1,11 +1,7 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-// import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 import { Box } from '@mui/material'
 
 import Header from '@/components/Header'
-
-// import TanStackQueryDevtools from '@/integrations/tanstack-query/devtools'
 
 import type { QueryClient } from '@tanstack/react-query'
 
@@ -18,18 +14,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     <Box>
         <Header />
         <Outlet />
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          // plugins={[
-          //   {
-          //     name: 'Tanstack Router',
-          //     render: <TanStackRouterDevtoolsPanel />,
-          //   },
-          //   TanStackQueryDevtools,
-          // ]}
-        />
     </Box>
   ),
 })
