6866f81162ab1790223527ccba5e5eb23710f950
Tyler Fink <tfink419@gmail.com>
2025-12-07 16:46:57 -0700

    Only reserve flights that arent full

--- airline-server/src/reservations/reservations.service.ts
+++ airline-server/src/reservations/reservations.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import {BadRequestException, Injectable} from '@nestjs/common';
 import {CreateReservationDto} from './dto/create-reservation.dto';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
@@ -16,10 +16,22 @@ export class ReservationsService {
     }
 
     async create(createReservationDto: CreateReservationDto, agentId?: number) {
+        const flight = await this.flightsRepository.createQueryBuilder('flight')
+            .leftJoin('flight.reservations', 'reservation')
+            .select('flight.*')
+            .addSelect('COUNT(reservation.id)::INTEGER', 'seatsReserved')
+            .where('flight.id = :id', {id: createReservationDto.flightId})
+            .groupBy('flight.id')
+            .addGroupBy('reservation.id')
+            .getRawOne();
+
+        if (flight.seatsReserved >= flight.seatsAvailable) throw new BadRequestException(
+            `Flight ${flight.flightCode} is full`
+        );
         const reservation = {
             ...createReservationDto,
             agentId,
-            flight: await this.flightsRepository.findOneByOrFail({id: createReservationDto.flightId}),
+            flight: flight,
             confirmationCode: Math.random().toString(36).substring(2, 7).toUpperCase()
         }
         const saved = await this.reservationsRepository.save(
